261def92eaca5d1564e49351da03f0f5a3958a8f
hendratmo <hendratmobudihardjo@gmail.com>
2025-06-24 16:02:09 +0800

    tambah .env

--- run.py
+++ run.py
@@ -1,20 +1,24 @@
+import os
 from app import create_app
 from create_db import create_admin_acct, create_themes, create_dummie_accts, create_posts, create_comments, create_stats, create_likes_and_bookmarks, create_contact_db
 from app.extensions import db
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-    create_admin_acct()
-    create_themes()
-    create_stats()
-    # the bellow is used to create dummie data and can be deleted, see createdb.py for more info
-    create_dummie_accts ()
-    create_posts()
-    create_comments()
-    create_likes_and_bookmarks()
-    create_contact_db()
+def seed_data():
+    with app.app_context():
+        db.create_all()
+        create_admin_acct()
+        create_themes()
+        create_stats()
+        if os.getenv("SEED_DUMMY", "false").lower() == "true":
+        # the bellow is used to create dummie data and can be deleted, see createdb.py for more info
+            create_dummie_accts ()
+            create_posts()
+            create_comments()
+            create_likes_and_bookmarks()
+            create_contact_db()
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    seed_data()
+    app.run(debug=os.getenv("FLASK_DEBUG", "1") == "1")
